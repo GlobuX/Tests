ee912a2dc22df0db0e8763cae8296a9600e70126
GlobuX <globuq@gmail.com>
2024-11-04 13:42:26 +0500

    URItest add output

--- FileSystemTest/src/java/ru/globux/test/net/URItest.java
+++ FileSystemTest/src/java/ru/globux/test/net/URItest.java
@@ -3,6 +3,7 @@ package ru.globux.test.net;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class URItest {
 
@@ -10,7 +11,21 @@ public class URItest {
 
         try {
             URI uri = new URI("mailto:ivanov@mail.ru");
-            System.out.println(uri.toURL().toString());
+            URL url = uri.toURL();
+            System.out.println(url.getProtocol());
+            System.out.println(url.getPath());
+
+            URL aURL = new URL("http://example.com:80/docs/books/tutorial"
+                    + "/index.html?name=networking#DOWNLOADING");
+
+            System.out.println("protocol = " + aURL.getProtocol());
+            System.out.println("authority = " + aURL.getAuthority());
+            System.out.println("host = " + aURL.getHost());
+            System.out.println("port = " + aURL.getPort());
+            System.out.println("path = " + aURL.getPath());
+            System.out.println("query = " + aURL.getQuery());
+            System.out.println("filename = " + aURL.getFile());
+            System.out.println("ref = " + aURL.getRef());
 
         } catch (URISyntaxException | MalformedURLException e) {
             e.printStackTrace();
