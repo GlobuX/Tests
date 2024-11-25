7ef45a62213453dad4aeb6841a26096af3174063
GlobuX <globuq@gmail.com>
2024-11-25 11:53:34 +0500

    add Test1.java

---
+++ jdkTest/src/java/ru/globux/test/unknown/Test1.java
@@ -0,0 +1,11 @@
+package ru.globux.test.unknown;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        Set<String> set = new TreeSet<>();
+    }
+}
